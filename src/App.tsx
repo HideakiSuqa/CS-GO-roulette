ee53fda2f5da364cbc81b9f869d0e81f5346f261
Muhitori <39372404+Muhitori@users.noreply.github.com>
2020-01-11 01:52:32 +0200

    merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 import Main from './Components/Main/Main';
-import Footer from './Components/Header/Footer';
-import Header from './Components/Footer/Header';
+import Header from './Components/Footer/Footer';
+import Footer from './Components/Header/Header';
 
 
 export default class App extends React.Component {
