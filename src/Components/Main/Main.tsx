97be9cc2527986d18c1c4290e661b2749d008c7b
HideakiSuqa <astilus1997@gmail.com>
2020-01-11 01:38:39 +0200

    test

--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -5,6 +5,19 @@ export default class Main extends React.Component {
   render(){
     return (
       <main>
+        <div className="container">
+  <div className="row">
+    <div className="col-sm">
+      One of three columns
+    </div>
+    <div className="col-sm">
+      One of three columns
+    </div>
+    <div className="col-sm">
+      One of three columns
+    </div>
+  </div>
+</div>
       </main>
     );
   }
