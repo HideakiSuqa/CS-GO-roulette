afc868e032dd56786ccd52e61aa0499457cbc227
Muhitori <39372404+Muhitori@users.noreply.github.com>
2020-01-11 01:55:19 +0200

    fuh

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -5,19 +5,7 @@ export default class Header extends React.Component {
   render(){
     return (
       <header>
-        <div className="container">
-  <div className="row">
-    <div className="col-sm">
-      One of three columns
-    </div>
-    <div className="col-sm">
-      One of three columns
-    </div>
-    <div className="col-sm">
-      One of three columns
-    </div>
-  </div>
-</div>
+
       </header>
     );
   }
--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -5,23 +5,6 @@ export default class Main extends React.Component {
   render(){
     return (
       <main>
-<<<<<<< HEAD
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-sm">
-              One of three columns
-            </div>
-            <div className="col-sm">
-              One of three columns
-            </div>
-            <div className="col-sm">
-              One of three columns
-            </div>
-          </div>
-        </div>
-=======
-      
->>>>>>> 29da0b34a042c9ccdfecacfc805f9964f2bdb4f9
       </main>
     );
   }
