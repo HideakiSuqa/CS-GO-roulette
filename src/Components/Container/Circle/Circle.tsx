09e45a4ced81075c24d2ea3cd06eb04bea4ff2f7
Muhitori <39372404+Muhitori@users.noreply.github.com>
2020-01-11 17:16:27 +0200

    nedokrug

--- src/Components/Container/Circle/Circle.tsx
+++ src/Components/Container/Circle/Circle.tsx
@@ -2,11 +2,20 @@ import React from 'react';
 import './Circle.css';
 
 export default class Circle extends React.Component {
-  render(){
-    return (
-      <div  className="col-4 box">
-            
-      </div>
-    );
+  componentDidMount() {
+    this.updateCanvas();
+}
+  updateCanvas() {
+      const c : any = this.refs.canvas;
+      const ctx = c.getContext('2d');
+      ctx.setLineDash([5,7]);
+      ctx.arc(100,75,50,0, 2*Math.PI);
+      ctx.stroke();
+
+  }
+  render() {
+      return (
+          <canvas className="circle col-md-4" ref="canvas" width={300} height={300}/>
+      );
   }
 }
