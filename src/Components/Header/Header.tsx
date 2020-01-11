4297e24b148924ca985de13a278954f7fe4392eb
HideakiSuqa <astilus1997@gmail.com>
2020-01-11 13:47:10 +0200

    css

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -4,8 +4,15 @@ import './Header.css';
 export default class Header extends React.Component {
   render(){
     return (
-      <header>
-
+      <header className="header">
+        <div className="header_container">
+          <div className="header_logo-wrap">
+            <a href="index.html" className="logo">CS:GO COIN</a>
+          </div>
+          <div className="header_singup-wrap">
+            <a href="#" className="singUp">Sing UP</a>
+          </div>
+        </div>
       </header>
     );
   }
---
+++ src/Components/Hero/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Hero.css';
+
+export default class Hero extends React.Component {
+  render(){
+    return (
+        <section className="hero">
+            saf
+        </section>
+    );
+  }
+}
