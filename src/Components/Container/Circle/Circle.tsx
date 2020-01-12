b5293c67a4e6d964bc3fd8a6de1e5537f158c669
Muhitori <39372404+Muhitori@users.noreply.github.com>
2020-01-12 03:48:59 +0200

    ладно, иду иду

--- src/Components/Container/Circle/Circle.tsx
+++ src/Components/Container/Circle/Circle.tsx
@@ -45,27 +45,56 @@ export default class Circle extends React.Component {
     // .attr("y2", d => { return d.target.y })
     // .style("stroke", "rgb(6,120,155)");
 
-    let data = [2, 8, 6, 10, 4, 5, 7, 3, 5, 6];
+    let data = [2, 8, 6, 10, 4, 2, 4, 6, 10, 4, 4, 6, 8, 6, 10, 4];
     let pie = d3.pie();
-    let g = chart.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+    let g = chart.append("g").attr("transform", "translate(" + width / 3 + "," + height / 3 + ")");
 
-    let color = d3.scaleOrdinal(['#4cffb5', '#b7cfff','#9665f1','rgba(0,0,0,0)','#f18aca','rgba(0,0,0,0)', '#4b5e9d']);
+    let color = d3.scaleOrdinal(['#4cffb5', 'rgba(0,0,0,0)',
+                                '#b7cfff','rgba(0,0,0,0)',
+                                '#9665f1','rgba(0,0,0,0)',
+                                '#f18aca','rgba(0,0,0,0)', 
+                                '#4b5e9d','rgba(0,0,0,0)']);
 
     let arc: any = d3.arc()
-                .innerRadius(93)
-                .outerRadius(100);
+                .innerRadius(87)
+                .outerRadius(95);
 
     let arcs = g.selectAll("arc")
                 .data(pie(data))
                 .enter()
                 .append("g")
                 .attr("class", "arc");
+                
+    var defs = chart.append("defs");
+
+    var filter = defs.append("filter")
+                .attr("id", "drop-shadow")
+            
+    filter.append("feGaussianBlur")
+                .attr("in", "SourceAlpha")
+                .attr("stdDeviation", 4)
+                .attr("result", "blur");
+    filter.append("feOffset")
+                .attr("in", "blur")
+                .attr("dx", 3)
+                .attr("dy", 3)
+                .attr("result", "offsetBlur");
+            
+    var feMerge = filter.append("feMerge");
+            
+    feMerge.append("feMergeNode")
+                .attr("in", "offsetBlur")
+    feMerge.append("feMergeNode")
+                .attr("in", "SourceGraphic");
+   
+    
 
     arcs.append("path")
                 .attr("fill", (d, i) => {
                     return color(i+"");
                 })
-                .attr("d", arc);
+                .attr("d", arc)
+                .attr("filter", "url(#drop-shadow)")
   }
 
   render() {
