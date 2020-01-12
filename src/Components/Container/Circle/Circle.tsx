c3b589425897c12eb1f999ea812bf3a3b6519ef4
Muhitori <39372404+Muhitori@users.noreply.github.com>
2020-01-12 02:20:48 +0200

    MWAHAHAHA

--- src/Components/Container/Circle/Circle.tsx
+++ src/Components/Container/Circle/Circle.tsx
@@ -22,8 +22,8 @@ export default class Circle extends React.Component {
     .enter()
     .append("circle")
     .attr("class", "nodes")
-    .attr("cx", function(d) { return d.x; })
-    .attr("cy", function(d) { return d.y; })
+    .attr("cx", d => { return d.x; })
+    .attr("cy", d => { return d.y; })
     .attr("r", "10px")
     .attr("fill", "black") 
     var links = [
@@ -34,10 +34,11 @@ export default class Circle extends React.Component {
     .data(links)
     .enter()
     .append("line")
-    .attr("x1", function(d) { return d.source.x })
-    .attr("y1", function(d) { return d.source.y })
-    .attr("x2", function(d) { return d.target.x })
-    .attr("y2", function(d) { return d.target.y })
+    .attr("stroke-width", 3)
+    .attr("x1", d => { return d.source.x })
+    .attr("y1", d => { return d.source.y })
+    .attr("x2", d => { return d.target.x })
+    .attr("y2", d => { return d.target.y })
     .style("stroke", "rgb(6,120,155)");
   }
 
