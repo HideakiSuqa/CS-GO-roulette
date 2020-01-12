8b55a4a06a4d1f68baee6ad433e61776519c211b
Muhitori <39372404+Muhitori@users.noreply.github.com>
2020-01-12 02:12:55 +0200

    МУХАХАХ

--- src/Components/Container/Circle/Circle.tsx
+++ src/Components/Container/Circle/Circle.tsx
@@ -3,21 +3,48 @@ import './Circle.css';
 import * as d3 from "d3";
 
 export default class Circle extends React.Component {
-  ref: SVGSVGElement | undefined;
+  //ref: SVGSVGElement | undefined;
   componentDidMount(){
-    d3.select("svg")
-      .append("circle")
-      .attr("r", 50)
-      .attr("cx", 1000 / 2)
-      .attr("cy", 1000 / 2)
-      .attr("fill", "red");
+    let chart = d3.select("#chart").append("svg");
+
+    let width = 1000,
+    height = 1000;
+
+    chart.attr("width", width)
+          .attr("height", height);
+
+    var nodes = [{x: 30, y: 50},
+                {x: 50, y: 80},
+                {x: 90, y: 120}]
+
+    chart.selectAll("circle .nodes")
+    .data(nodes)
+    .enter()
+    .append("circle")
+    .attr("class", "nodes")
+    .attr("cx", function(d) { return d.x; })
+    .attr("cy", function(d) { return d.y; })
+    .attr("r", "10px")
+    .attr("fill", "black") 
+    var links = [
+              {source: nodes[0], target: nodes[1]},
+              {source: nodes[2], target: nodes[1]}
+      ]
+    chart.selectAll(".line")
+    .data(links)
+    .enter()
+    .append("line")
+    .attr("x1", function(d) { return d.source.x })
+    .attr("y1", function(d) { return d.source.y })
+    .attr("x2", function(d) { return d.target.x })
+    .attr("y2", function(d) { return d.target.y })
+    .style("stroke", "rgb(6,120,155)");
   }
 
   render() {
       
       return (
-          <svg className="container" width={1000} height={1000}>
-          </svg>
+        <div id="chart"></div>
       );
   }
 }
